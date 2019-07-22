151db29eb321a2caf7f3eba097a3c03ac82a39e0
te68 <te68@cornell.edu>
2019-07-22 10:56:56 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,46 @@ const questions = [
 ]
 
 let question;
+const questionContainer = document.querySelector('.question-container')
+
+function appendQuestion(question) {
+  questionContainer.innerText = question.questionText
+}
+
+function askQuestionThen(time) {
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise((resolve, reject) => {
+    setTimeout(function(){
+    removeQuestion()
+    }, time )
+  })
+}
+
+function removeQuestion() {
+  questionContainer.innerText = ""
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  return askQuestionThen(time).then(removeQuestion)
+}
+
+function trueAndFalseButtons() {
+  const buttons = document.querySelector('.true-false-list').querySelectorAll('.btn')
+  return buttons
+}
+
+function toggleTrueAndFalseButtons() {
+trueAndFalseButtons().forEach(function(button) {
+  if (button.classList.contains('hide')) {
+      button.classList.remove('hide')
+  }
+  else {
+    button.classList.add('hide')
+  }
+})
+}
+
+function displayQuestionOnClick() {
+  askQuestionThenRemoveQuestion(5000)
+}
